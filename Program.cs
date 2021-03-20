f7243a527955c749926e031fc569726a316f3b2c
Brandon Utrup <47999059+butrup96@users.noreply.github.com>
2021-03-20 13:03:47 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace numberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Initializes random generator
+            Random r = new Random();
+
+            //winning number is chosen
+            int winNum = r.Next(1, 100);
+
+            //needed for loop
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Enter a number between 1 an 100");
+                string input = Console.ReadLine();
+
+                int i = int.Parse(input);
+
+                //too high
+                if (i > winNum)
+                {
+                    Console.WriteLine("Too High! Go Lower!");
+                }
+
+                //too low
+                else if( i < winNum)
+                {
+                    Console.WriteLine("Too Low! Go Higher!");
+                }
+
+                //correct guess
+                else if(i == winNum)
+                {
+                    Console.WriteLine("That's it! You Win!");
+                    win = true;
+                }
+                //formating line
+                Console.WriteLine();
+            } while (win == false);
+
+            //needs input to finish program
+            Console.WriteLine("Press and key to exit");
+            Console.ReadKey(true);
+        }
+    }
+}
